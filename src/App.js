9a4a84a97c19c04d57456b1f1a7521ec3e8edd51
Aydan <aydan2903@gmail.com>
2022-03-01 19:18:06 +0400

    starting project

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import Error from './pages/Error'
+import Login from './pages/Login'
+import Main from './pages/Main'
+
+
+function App() {
+  return (
+   <Router>
+     <Switch>
+       <Route path="/" exact>
+         <Main/>
+       </Route>
+       <Route path="/login">
+         <Login/>
+       </Route>
+       <Route path="*">
+         <Error/>
+       </Route>
+     </Switch>
+   </Router>
+  );
+}
+
+export default App;
