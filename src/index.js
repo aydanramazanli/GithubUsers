17a2e436c24fb5a4ca10d528c483ccfd1e3c5a21
Aydan <aydan2903@gmail.com>
2022-03-05 22:03:33 +0400

    added auth0 fb twitter and gmail

--- src/index.js
+++ src/index.js
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {GithubProvider} from './context/Context'
-
+import { Auth0Provider } from "@auth0/auth0-react";
+// dev-ulb0zt0i.us.auth0.com   domain
+// AnpismIGzOxk1nqHqPxjF6gtRfVK5iUh   client id
+// YMNr6lIqM5TGFqvV6T1BcD1_BXwG5ysIvG61kb3pVHIg5ibPoZhVvvL29dsM9TSH  client secret
 
 ReactDOM.render(
   <React.StrictMode>
+    <Auth0Provider
+     domain="dev-ulb0zt0i.us.auth0.com"
+     clientId="AnpismIGzOxk1nqHqPxjF6gtRfVK5iUh"
+     redirectUri={window.location.origin}>
     <GithubProvider>
     <App />
     </GithubProvider>
+    </Auth0Provider>
+  
   
   </React.StrictMode>,
   document.getElementById('root')
